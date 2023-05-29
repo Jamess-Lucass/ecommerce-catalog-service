87982c5661b77833d9f8bdfeef6fb3797162df19
Jamess-Lucass <jameslucas1210@gmail.com>
2023-05-29 21:56:41 +0100

    feat: implemented goatquery

--- handlers/catalog.go
+++ handlers/catalog.go
@@ -4,18 +4,38 @@ import (
 	"github.com/Jamess-Lucass/ecommerce-catalog-service/models"
 	"github.com/Jamess-Lucass/ecommerce-catalog-service/requests"
 	"github.com/Jamess-Lucass/ecommerce-catalog-service/utils"
+	"github.com/goatquery/goatquery-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
 
+var maxTop = 1_000
+
 // GET: /api/v1/catalog
 func (s *Server) GetAllCatalogItems(c *fiber.Ctx) error {
-	var items []models.Catalog
-	if err := s.catalogService.List().Find(&items).Error; err != nil {
-		return c.Status(400).JSON(fiber.Map{"code": 400, "message": err.Error()})
+	query := goatquery.Query{
+		Top:     c.QueryInt("top", 0),
+		Skip:    c.QueryInt("skip", 0),
+		Count:   c.QueryBool("count", false),
+		OrderBy: c.Query("orderby"),
+		Select:  c.Query("select"),
+		Search:  c.Query("search"),
+		Filter:  c.Query("filter"),
 	}
 
-	return c.Status(200).JSON(items)
+	var items []models.Catalog
+	res, count, err := goatquery.Apply(s.catalogService.List(), query, &maxTop, nil)
+	if err != nil {
+		return c.Status(400).JSON(goatquery.QueryErrorResponse{Status: 400, Message: err.Error()})
+	}
+
+	if err := res.Find(&items).Error; err != nil {
+		return c.Status(400).JSON(goatquery.QueryErrorResponse{Status: 400, Message: err.Error()})
+	}
+
+	response := goatquery.BuildPagedResponse(items, query, count)
+
+	return c.Status(200).JSON(response)
 }
 
 // GET: /api/v1/catalog/1
--- handlers/routes.go
+++ handlers/routes.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"os"
+	"strings"
+
 	"github.com/Jamess-Lucass/ecommerce-catalog-service/middleware"
 	"github.com/Jamess-Lucass/ecommerce-catalog-service/requests"
 	"github.com/gofiber/contrib/fiberzap"
@@ -12,7 +15,14 @@ func (s *Server) Start() error {
 	s.validator.RegisterStructValidation(requests.CreateCatalogItemRequestValidation, requests.CreateCatalogItemRequest{})
 
 	f := fiber.New()
-	f.Use(cors.New(cors.Config{AllowOrigins: "*", AllowCredentials: true, MaxAge: 0}))
+	f.Use(cors.New(cors.Config{
+		AllowOrigins: os.Getenv("CORS_ALLOWED_ORIGINS"),
+		AllowOriginsFunc: func(origin string) bool {
+			return strings.EqualFold(os.Getenv("ENVIRONMENT"), "development")
+		},
+		AllowCredentials: true,
+		MaxAge:           0,
+	}))
 
 	f.Use(fiberzap.New(fiberzap.Config{
 		Logger: s.logger,
